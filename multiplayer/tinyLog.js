4b7a739a604bbebafe08478936003cfd77860c1b
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-05 12:19:46 -0300

    more tiny log

---
+++ multiplayer/tinyLog.js
@@ -0,0 +1,16 @@
+const moment = require('moment-timezone');
+module.exports = function (msg, name, name2) {
+
+	const now = moment().format('MM/DD/YYYY HH:mm:ss');
+
+	if (typeof name === 'string') {
+		if (typeof name2 === 'string') {
+			return `[${now}] [${name}] [${name2}] ${msg}`;
+		} else {
+			return `[${now}] [${name}]  ${msg}`;
+		}
+	} else {
+		return `[${now}]  ${msg}`;
+	}
+
+};
