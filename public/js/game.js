b1ee3c82a675dbd55107361b507dfaeb00debded
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-05 11:41:11 -0300

    jQuery

--- public/js/game.js
+++ public/js/game.js
@@ -1,7 +1,7 @@
 gameCache.game = async function(Scene, maze) {
 	
 	await maze.loadAssets();
-	maze.start(document.getElementById("canvas"));
+	maze.start($('#canvas')[0]);
 	maze.instantiate(Scene);
 	
 	return gameCache.start(Scene, maze);
