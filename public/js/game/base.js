64bb85244db76b2e3717674bfbb8280ae250d97b
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-05 12:38:24 -0300

    game menu

--- public/js/game/base.js
+++ public/js/game/base.js
@@ -1,9 +1,33 @@
-gameCache.game = async function (Scene, maze) {
+gameCache.game = function (Scene, maze) {
 
-	await maze.loadAssets();
-	maze.start($('#canvas')[0]);
-	maze.instantiate(Scene);
+	tinyLib.modal({
 
-	return gameCache.start(Scene, maze);
+		id: 'start_game',
+
+		title: 'Maze Game',
+		dialog: 'modal-lg',
+
+		body: $('<center>').append(
+			$('<h3>').text('Welcome to Maze!'),
+		),
+
+		footer: [
+			$('<button>', { class: 'btn btn-primary' }).text('Start Game').click(async function() {
+				
+				$('#start_game').modal('hide');
+				$.LoadingOverlay('show', {background: 'rgba(0,0,0, 0.5)'});
+
+				await maze.loadAssets();
+				maze.start($('#canvas')[0]);
+				maze.instantiate(Scene);
+
+				$.LoadingOverlay('hide');
+			
+				return gameCache.start(Scene, maze);
+
+			})
+		]
+
+	});
 
 };
