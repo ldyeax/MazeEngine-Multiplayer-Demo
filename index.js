8c6084f068b35f3e79dc7cc3ee78caa1881d4aa0
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-03-04 23:00:00 -0300

    index update

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
+// Modules
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const server = http.createServer(app);
 
+// Prepare Node App
 const app = express();
 const port = 3001;
 
+// Test node Page
+app.get('/tiny-test', (req, res) => {
+    res.send('<h1>Tiny Hello world. :3</h1>');
+});
+
+app.get('/tiny-test-2', (req, res) => {
+    res.sendFile(path.join(__dirname, './private/index.html'));
+});
+
+// Static Files
 app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(port, () => {
+// Start Server
+server.listen(port, () => {
     console.log(`Test app listening on port ${port}`);
 });
